bd8f0469c44766f3a9f3155fea275c1a456c8758
Rade Milovic <r.milovic90@gmail.com>
2025-12-01 23:26:32 +0100

    Add tests for dial rotation to the left/right.

--- 01-secret-entrance/Safe.cs
+++ 01-secret-entrance/Safe.cs
@@ -2,5 +2,17 @@ namespace SecretEntrance;
 
 internal sealed class Safe
 {
-    public uint DialPosition { get; } = 50;
+    public uint DialPosition { get; private set; } = 50;
+
+    public void TurnDial(IEnumerable<string> rotations)
+    {
+        string direction = rotations.First()[0..1];
+        uint distance = uint.Parse(rotations.First()[1..]);
+
+        if (direction == "L")
+            DialPosition -= distance;
+
+        if (direction == "R")
+            DialPosition += distance;
+    }
 }
--- 01-secret-entrance/SafeTests.cs
+++ 01-secret-entrance/SafeTests.cs
@@ -9,4 +9,24 @@ public sealed class SafeTests
 
         await Assert.That(safe.DialPosition).IsEqualTo(50u);
     }
+
+    [Test]
+    public async Task Dial_rotation_to_the_left_moves_the_dial_towards_lower_numbers()
+    {
+        Safe safe = new ();
+
+        safe.TurnDial(["L10"]);
+
+        await Assert.That(safe.DialPosition).IsEqualTo(40u);
+    }
+
+    [Test]
+    public async Task Dial_rotation_to_the_right_moves_the_dial_towards_higher_numbers()
+    {
+        Safe safe = new ();
+
+        safe.TurnDial(["R10"]);
+
+        await Assert.That(safe.DialPosition).IsEqualTo(60u);
+    }
 }
